0bcef9a5cb65569683e71d6b15add6ce379721cb
rika-tula <ricazabala04@gmail.com>
2024-12-13 10:20:45 -0300

    add a personage list

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,47 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useEffect, useState } from 'react';
 import './App.css'
 
+interface Personage {
+  id: number;
+  name: string;
+  image:string;
+  species: string
+}
+
 function App() {
-  const [count, setCount] = useState(0)
+
+  const [personages, setPersonages] = useState<Personage[]>([])
+
+  useEffect(() =>{
+    fetch("https://rickandmortyapi.com/api/character").then((response)=>{
+      if (!response) {
+        throw new Error()
+      }
+      return response.json()
+    })
+    .then((data) =>{
+      setPersonages(data.results)
+    })
+    .catch((error) =>{
+      console.log(error);
+      
+    })
+  })
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+       <div className="app-container">
+      <h1>Rick y Morty Personajes</h1>
+      <div className="cards-container">
+        {personages.map((p, i) => (
+          <div key={i} className="card">
+            <img src={p.image} alt={p.name} />
+            <h2>{p.name}</h2>
+            <p>{p.species}</p>
+          </div>
+        ))}
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    </div>
     </>
   )
 }
